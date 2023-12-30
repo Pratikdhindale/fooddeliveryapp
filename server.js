20af760ea2abbc46ac4112a934567c5a1e7d1c67
Pratikdhindale <142910907+Pratikdhindale@users.noreply.github.com>
2023-12-30 12:45:47 +0000

    updating changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 5000
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 app.listen(port, () => {
